922a29670bf273fbd2ed01981755aa7a66432191
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-03-08 11:41:34 +0300

    b189

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,48 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-
+    double result = value;
+    for (uint16_t i = 1; i < n; i++) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    double result;
+    result = pown(x, n) / fact(n);
+    return result;
 }
 
 double expn(double x, uint16_t count) {
-
+    double result = 1;
+    for (uint16_t i = 1; i <= count; i++) {
+        result += calcItem(x, i);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double result = x;
+    for (uint16_t i = 2; i <= count; i++) {
+    result += pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double result = 1;
+    for (uint16_t i = 2; i <= count; i++) {
+        result += pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
+    }
+    return result;
 }
